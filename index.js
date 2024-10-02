74092fd3f41a40a5c6dd77d57f2ebea65b7e6f1c
iamasif <ajazhussainhussain8815@gmail.com>
2024-10-02 10:15:51 +0530

    Resolving merge conflicts

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
+
 const db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -53,6 +54,17 @@ const db = new pg.Pool({
 db.connect()
 .then(() => console.log("Connected to the database"))
 .catch(err => console.error("Connection error", err.stack));
+
+
+/*const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book_hub",
+    password: "Qwert..",
+    port: 5432,
+ });
+
+ db.connect();*/
 
 
 
